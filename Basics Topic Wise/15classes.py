ebd002ab70c60ad58df45bd8a52a988460bd4d4e
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-11 14:30:20 -0600

    Added the inheritance topic with the example and code

--- Basics Topic Wise/15classes.py
+++ Basics Topic Wise/15classes.py
@@ -24,3 +24,27 @@ print(p1)    # you can access each attribute of the object p1 by calling p1.name
         print("Age is : "+str(self.age))
         print("The city is : "+ self.city)
         print("The language is : "+ self.language)"""
+
+
+#The string representation of an object WITHOUT the __str__() function:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+#The string representation of an object WITH the __str__() function:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
---
+++ Basics Topic Wise/16Inheritance.py
@@ -0,0 +1,13 @@
+#Inheritance allows us to define a class that inherits all the methods and properties from another class.
+#Parent class is the class being inherited from, also called base class. Child class is the class that inherits from another class, also called derived class.
+
+class Person :
+    def __init__(self, fname,lname):
+        self.fname = fname
+        self.lname = lname
+    def printName(self):
+        print(self.fname, self.lname)
+
+p1 = Person("Chenchu", "Reddy")
+p1.printName() 
+
