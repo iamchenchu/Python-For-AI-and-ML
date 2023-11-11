4e57aa863aeb1e42adbe22ab0a8a71c62cb28474
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-11 11:22:56 -0600

    updated the topic of classes and objects in oops

---
+++ Basics Topic Wise/15classes.py
@@ -0,0 +1,26 @@
+#Python is an object oriented programming language.
+#Almost everything in Python is an object, with its properties and methods. A Class is like an object constructor, or a "blueprint" for creating objects.
+#The self parameter is a reference to the current instance of the class, and is used to access variables that belong to the class.
+
+class Person :
+    def __init__(self,name,age,city,language): 
+        self.name = name
+        self.age = age
+        self.city = city
+        self.language = language
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, City: {self.city}, Language: {self.language}"
+
+p1 = Person("Chenchu",25,"Tirupati","Telugu")
+
+print(p1)    # you can access each attribute of the object p1 by calling p1.name or p1.age but
+             #to print all the attributes at time weneed to use __str__() method to convert all the object attributes to strings
+
+
+
+"""def display(self):
+        print("Name is : "+ self.name)
+        print("Age is : "+str(self.age))
+        print("The city is : "+ self.city)
+        print("The language is : "+ self.language)"""
