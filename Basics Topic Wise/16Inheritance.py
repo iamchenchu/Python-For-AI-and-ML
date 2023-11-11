7c85ecef45e2a22c995e7afa211e0832c46ace3c
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-11 16:33:39 -0600

    Added the inheritance topic with the example and code

--- Basics Topic Wise/16Inheritance.py
+++ Basics Topic Wise/16Inheritance.py
@@ -1,7 +1,7 @@
 #Inheritance allows us to define a class that inherits all the methods and properties from another class.
 #Parent class is the class being inherited from, also called base class. Child class is the class that inherits from another class, also called derived class.
 
-class Person :
+class Person :                                 # this is a base class and parent class
     def __init__(self, fname,lname):
         self.fname = fname
         self.lname = lname
@@ -9,5 +9,22 @@ class Person :
         print(self.fname, self.lname)
 
 p1 = Person("Chenchu", "Reddy")
-p1.printName() 
+p1.printName()
 
+class Student(Person):
+    pass                                     # we can write pass keyword here if we don't have any attributes or methods to define 
+
+x = Student("Mike","Olsen")
+x.printName()                                 # we did not initialize any methods or attributes in the child class but still it is taking the help from parent and executing the code
+
+# When you add the __init__() function, the child class will no longer inherit the parent's __init__() function.
+#By using the super() function, you do not have to use the name of the parent element, it will automatically inherit the methods and properties from its parent.
+
+class Employee(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+
+e = Employee("Rajia", "Rani")
+e.printName()
+
+    
