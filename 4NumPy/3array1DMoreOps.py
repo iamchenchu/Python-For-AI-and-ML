d6e0875fb89c219b1e39b0ce14e5852ce8a8c5cd
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-15 11:09:42 -0600

    added codes for Numpy

---
+++ 4NumPy/3array1DMoreOps.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+a = np.array([0,1,2,3,4,5,6,78,9,10])   
+print(a)
+
+print(a.size)                     # prints the size of the array
+print(a.sum())                    # prints the sum of the all elements
+print(a.max())                    # prints the max of the all elements
+print(a.mean())                   # prints the mean of all the elements
+print(a.shape)                    # The attribute shape is a tuple of integers indicating the size of the array in each dimension
+print(a.min())                    # prints the min value in the elements
+print(a.ndim)                     # prints the number of the array dimentions or rank of the array
+print(a.std())                    # prints the standard deviation of the array
+
---
+++ 4NumPy/4arrayArithmeticOps.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+array1 = np.array([4,2,3,65,23,7,5,98])
+array2 = np.array([2,3,4,1,2,34,6,7])
+
+mult = array1 * array2       # we are performing the multiplication here
+print(mult)
+
+add = array1 + array2        # we are performing the addition here
+print(add)
+
+substract = array1 - array2   # substraction
+print(substract)
+
+divi = array2/array2          # 
+print(divi)
+
+
+
