ff767a4a993b2feba5a1986c1c7534a809da767e
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-15 09:28:29 -0600

    added the code for numpy

--- 4NumPy/2array1DOperations.py
+++ 4NumPy/2array1DOperations.py
@@ -7,5 +7,19 @@ print(a)
 
 a[0] = 100                  # assigning the new value to the array 
 print(a)                    # array being printed using the new assigned value 
-print(type(a))              # the type of the array will be printed
+print(type(a))              # the type of the array will be printed as ndarray
+print(a[1:6])               # prints all the elements from 1st to till 5
+print(a[1:5:2])             # prints the elements from 1 to 4th with 2 as range as gap 
+print(a[:4])                # prints all the elements till 3 index means 4 elements
+print(a[4:])                # prints all the elements form the 4th element
+print(a[1:5:])              # even though we don't mention the range it consider the default as 1 and prints from 1st index to till 4th
+print(a[1:8:2])             # prints all the even index elements
+
+
+
+list =np.array([1,2,6,4,7,98,14])     # creation of new list
+print(list)                         # printing the list
+indexlist = [0,1,3,4]               # 
+c = list[indexlist]                    # passing select list as an index of list
+print(c)
 
