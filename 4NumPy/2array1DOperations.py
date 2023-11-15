d3ce195da0a1eccdf7808de730c6790ce205d697
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-11-15 08:20:36 -0600

    Added the code for numpy

---
+++ 4NumPy/2array1DOperations.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+a = np.array([32,12,54,76,23,54,23,64,98,18,25])      # created the 1D array
+
+print(a)
+
+a[0] = 100                  # assigning the new value to the array 
+print(a)                    # array being printed using the new assigned value 
+print(type(a))              # the type of the array will be printed
+
