7b99e61754691743cea1f2a4529ee8419151ca9c
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-12 08:32:44 -0600

    Updated the OOP's concept in python

--- 00OOP's/04studentclass.py
+++ 00OOP's/04studentclass.py
@@ -1,11 +1,20 @@
 class Student:
-    def __init__(self, name, house):    #Initializing the attributes of the class it is also called as dundur method or magic method
+    def __init__(self, name, house):    #Initializing the attributes of the object the class it is also called as dundur method or magic method
         self.name = name
         self.house = house
 
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+"""Object Creation:
+
+When you create a new instance of a class, Python automatically calls the __new__ method to create the object.
+After the object is created, the __init__ method is called to initialize the object's attributes.
+"""
+
 def main ():
     student = get_student()
-    print(f"{student.name} from {student.house}")       
+    print(student)      
 
 
 def get_student():
