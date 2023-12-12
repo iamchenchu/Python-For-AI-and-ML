5194aacadf90275fbaeddbaabb6cec5618260689
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-11 22:29:10 -0600

    Renamed the file with numbering

---
+++ 09MathTopics/1vectorOps.py
@@ -0,0 +1,9 @@
+def vectorAddition(v1, v2):
+    if len(v1) != len(v2):
+        raise ValueError ("Vectors must have same length to perform the task")
+    
+    c = [v1+v2 for v1, v2 in zip(v1, v2)] # list comprehension is being used
+    #c = v1+v2 not working properly 
+
+    return c
+print (vectorAddition([8.218, -9.341],[-1.129, 2.111]))
