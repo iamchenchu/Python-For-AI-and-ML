dea34bfc08ace2c639ce39b6b4c5600e86ed5eba
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-11 22:42:27 -0600

    Added code for OOP's concepts

---
+++ 00OOP's/00student.py
@@ -0,0 +1,14 @@
+def main ():
+    name = input("Name : ")
+    house = input("House : ")
+    print(f"{name} from {house}")
+
+
+"""def get_name():
+    return input("Name : ")
+
+def get_house():
+    return input("House : ")"""
+
+if __name__ == "__main__":
+    main()
