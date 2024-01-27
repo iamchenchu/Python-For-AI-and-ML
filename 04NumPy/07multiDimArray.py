190cafad3ed2c853c12bf43d569c5cdeb7daaa4c
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-27 16:07:26 -0600

    Added the code for numpy

---
+++ 04NumPy/07multiDimArray.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3],[3, 9, 3]])
+print(arr)
+
+one_dimen = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+#convert one dimention to multi dimention
+multi_dimen = np.reshape(one_dimen,
+                         (2,4)) 
+print(one_dimen)
+print(multi_dimen)
+
+# Finding size, shape and dimension.
+print(multi_dimen.ndim)
+print(multi_dimen.shape)
+print(multi_dimen.size)
