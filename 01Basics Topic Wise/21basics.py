22fecf91aad3e129256fd5730d3454a95a83b767
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-09 22:06:53 -0600

    Added the code

---
+++ 01Basics Topic Wise/21basics.py
@@ -0,0 +1,37 @@
+# # Symbol will be used to comments, for single comment 
+# python --version = will print the version of python 
+# python helloworld.py = to run the the code 
+
+"""this is the multi level comment 
+this is the multi level comment
+this is the multi level comment"""
+print("Hello world")
+
+# Variable names correct and accepted 
+my_name = "chenchu"
+Myname = "reddy"
+my_Name = "Chenchaiah"
+_my_name = "chenchu"
+var2 = "chenchu"
+my_variable_name = "John"
+
+print(my_name, Myname, my_name, _my_name, my_variable_name)
+
+# wrong variable names
+"""2var = "chenchu"
+my-name = "ram"
+my var = "bheem"
+"""
+
+#Multiple assignments
+x, y, z = "Orage", "Blue", "Red"
+print(x)
+print(y)
+print(z)
+
+# Unpack the list
+team = "vinod", "rama", "sashank", "vinay", "mano"
+a, b, c, d, e = team
+print(team)
+
+
