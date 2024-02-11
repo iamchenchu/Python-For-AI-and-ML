bfc89a84249554cf0200157045a0fbef03f02ab7
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-11 09:24:50 -0600

    Added the code

---
+++ 01Basics Topic Wise/27tupples.py
@@ -0,0 +1,15 @@
+# used to store sequence of data into one variable 
+# it's ordered and unchageble 
+# tuples are written using round brackets 
+
+team = ("ram", "chenchu", "bheem", "krishna", "jalaal")
+print(team)
+
+
+print(len(team))
+print(team[1])
+print(team[-1])
+
+print(team.count("ram"))
+print(team.index("bheem"))
+print(type(team))
