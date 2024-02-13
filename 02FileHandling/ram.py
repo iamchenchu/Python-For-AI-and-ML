5defb6ab9dc98e526795098516703081e86ff84f
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-13 15:43:12 -0600

    Added the code

--- 02FileHandling/ram.py
+++ 02FileHandling/ram.py
@@ -1,19 +1,19 @@
 # Tuples 
-team_chenchu = ("vinodh", "prashanth", "ramakrishna", "dumpala",["rama", "edara"], "vinodh")
+team_chenchu = ("vinodh", "prashanth", "ramakrishna", "dumpala","rama", "edara", "vinodh")
 
 
 print(team_chenchu)
+print(team_chenchu[2])
 print(team_chenchu[0])
 print(team_chenchu[-1][0])
 print(team_chenchu[-2])
 print(team_chenchu[-4])
 print(team_chenchu.count("vinodh"))
 print(team_chenchu.index("vinodh"))
-
 print(type(team_chenchu))
-
 team_ram = set(team_chenchu)
 print(team_ram)
+
 
 
 
