a91e9fe5ac4cd1970c47e64edfe33276e79918bb
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-18 17:00:55 -0600

    Added the code

---
+++ 00OOP's/basics.py
@@ -0,0 +1,20 @@
+a = 100
+b = 90
+
+if a > 0 :
+  print("A is bigger than 50")
+if a > 30 :
+  print("A is bigger than 30")
+if a > 40 :
+  print("A is bigger than 40")
+if a > 70 :
+  print("A is bigger than 30")
+else:
+  print("I have programming")
+
+
+
+
+
+
+
---
+++ 01Basics Topic Wise/34functions.py
@@ -0,0 +1,10 @@
+names = ["Ram", "Bheem", "Pandu", "Vijay", "Ajay", "Shammi", "Vinodh", "Vinay", "Hemanth", "Prudhvi", "Govindhi"]
+scores = [89, 85, 93, 96, 92, 100, 75, 30, 47, 40, 94]
+
+def make_dict(names, marks):
+    sudents_dict = dict(zip(names, marks))
+    print(sudents_dict)
+
+
+make_dict(names, scores)
+
