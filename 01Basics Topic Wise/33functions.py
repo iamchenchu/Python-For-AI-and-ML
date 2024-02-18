d45560dffb853898db451f7007fe909d8f3ab15d
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-18 10:24:04 -0600

    Added the python code

---
+++ 01Basics Topic Wise/33functions.py
@@ -0,0 +1,8 @@
+def my_function(child1, child2, child3):
+    print("My youngest child is : ",child3)
+    print("My elder child is : ", child1)
+    print("My middle child is : ", child2)
+
+
+
+my_function("Ram", "Bheem", "Bharath")
