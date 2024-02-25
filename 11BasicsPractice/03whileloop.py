7be0dfebff95f4a0445ba0cbb0ca8d4a036d5a2e
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-25 08:34:43 -0600

    Added the code for lambda

--- 11BasicsPractice/03whileloop.py
+++ 11BasicsPractice/03whileloop.py
@@ -4,4 +4,7 @@ while i < 10 :
     if i == 5:
         continue
     print(i)
+
+
+    
     
---
+++ 11BasicsPractice/05lambda.py
@@ -0,0 +1,11 @@
+"""
+A lambda fucntion is small anonymous function
+A lambda function can take any number of arguments but can have only one expression
+"""
+
+x = lambda a : a+10
+print(x(10))
+
+c = lambda a, b : a + b
+print(c(10, 20))
+print(c(20, 5))
