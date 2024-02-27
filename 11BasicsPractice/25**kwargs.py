b07c0fd99222ff813a251f6421fbe56180acb57f
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-27 15:12:56 -0600

    Added the code

---
+++ 11BasicsPractice/25**kwargs.py
@@ -0,0 +1,5 @@
+def func(*args, **kwargs):
+    print("Positional arguments:", args)  # *args means : u can give any dynamic number of arguments of same variables
+    print("Keyword arguments:", kwargs) # **kwargs means : u can define any number of variables in the method dynamically
+
+func(1, 2, 3, a='foo', b='bar', c =30)
