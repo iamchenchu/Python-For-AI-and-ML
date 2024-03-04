31c562599766fbb05ffca2f7d6b14fb9f5a5ca02
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-04 11:22:36 -0600

    Added the code of Dict

---
+++ 01Basics Topic Wise/39dict.py
@@ -0,0 +1,18 @@
+my_dict = {'a': 1, 
+           'b': 2, 
+           'c': 3}
+
+print(my_dict)
+
+list1 = list(my_dict)
+print(list1)
+
+list2 = list(my_dict.values())
+print(list2)
+
+tuple1 = tuple(my_dict.keys())
+print(tuple1)
+
+tuple2 = tuple(my_dict.values())
+print(tuple2)
+
--- 01Basics Topic Wise/hi.py
+++
@@ -1,5 +0,0 @@
-list4 = [50, 60, 70, 40, 50, 30]
-
-list5 = list4.sort()
-
-print(list5)
