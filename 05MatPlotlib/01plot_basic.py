cf20714c72e2be25200fe393517da937f2029faa
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-07 09:10:20 -0600

    Added the code

--- 05MatPlotlib/01plot_basic.py
+++ 05MatPlotlib/01plot_basic.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-arr1 = np.array([5, 9, 19])
-arr2 = np.array([100, 150, 200])
+arr1 = np.array([5, 9, 19]) # Numpy
+arr2 = np.array([100, 150, 200]) # Numpy
 
-plt.plot(arr1, arr2) # plot(x_axis, y_axis)
-plt.show()
+plt.plot(arr1, arr2) # plot(x_axis, y_axis) # Matplotlib
+plt.show() # Matplotlib
--- 05MatPlotlib/04plot.py
+++ 05MatPlotlib/04plot.py
@@ -1,8 +1,8 @@
 """If we do not specify the points on the x-axis, they will get the default values 0, 1, 2, 3 etc., depending on the length of the y-points.
 So, if we take the same example as above, and leave out the x-points, the diagram will look like this:"""
 
-import matplotlib.pyplot as plt 
-import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np 
 
 x = np.array([4, 2, 7, 19])
 
