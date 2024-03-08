4e2750e90e3ed723724296bbd3adec6c92feced9
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-08 11:35:40 -0600

    Added the code

--- 00Python By AC/02ObjectOrientedProgramming/19property_decorator.py
+++ 00Python By AC/02ObjectOrientedProgramming/19property_decorator.py
@@ -3,3 +3,18 @@ Property Decorator : We use @property decorator on any method in the class to us
 
 """
 
+class Student:
+    def __init__(self, phy, chem, math):
+        self.physics = phy
+        self.chemistry = chem
+        self.maths = math
+        self.percentage = str((self.physics + self.chemistry + self.maths)/3) + "%"
+
+
+student1 = Student(92, 95, 98)
+print(student1.percentage)
+
+
+student1.chemistry = 100 # Changing the object value 
+ 
+print(student1.percentage)   # it's not changing the percentage as it's not getting effected by the object modification
