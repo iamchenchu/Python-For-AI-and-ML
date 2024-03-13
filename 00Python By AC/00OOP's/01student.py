fa8406ce002cab6a3671d76b77b04b6e9af155bb
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-13 14:02:26 -0500

    Added the code

--- 00Python By AC/00OOP's/01student.py
+++ 00Python By AC/00OOP's/01student.py
@@ -5,15 +5,17 @@ def main ():
     print(f"{student[0]} from {student[1]}")
 
 
-def get_student():
-    name = input("Name :")
-    house = input("House :")
-    return (name, house)                 # We are returning the tuple here as we usually don't change the content (tuple is immutable)
 
 def get_student():
     name = input("Name :")
     house = input("House :")
     return [name, house]                 # If explicityly we want to change the content sometimes then we use the list which is mutable 
 
+def get_student():
+    name = input("Name :")
+    house = input("House :")
+    return (name, house)                 # We are returning the tuple here as we usually don't change the content (tuple is immutable)
+
 if __name__ == "__main__":
     main()
+    
